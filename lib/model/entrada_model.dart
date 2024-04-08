637e06fa05a3f2ca2f3be0428ef2062e4e69571b
Richard Gusmao <richardgusmaoalberto@gmail.com>
2024-04-08 22:42:06 +0100

    Create entrada_model.dart

---
+++ lib/model/entrada_model.dart
@@ -0,0 +1,24 @@
+import 'package:minhas_financas/model/base_dados.dart';
+
+class Entrada {
+  int? id;
+  String? nome;
+  double? valor;
+  DateTime? dataHora;
+
+  Entrada({this.nome, this.valor, this.dataHora, this.id});
+  Future<void> adicionar() async {
+    final db = await BaseDados().conectar();
+    await db.insert("entrada", {
+      'nome': nome,
+      'valor': valor,
+      'dataHora': dataHora.toString(),
+    });
+  }
+
+  Future<List> obterTodos() async {
+    final db = await BaseDados().conectar();
+    List data = await db.query("entrada");
+    return data;
+  }
+}
