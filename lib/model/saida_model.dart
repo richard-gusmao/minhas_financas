82eba4c7597c40c0c2458877faae8a267f07d47e
Richard Gusmao <richardgusmaoalberto@gmail.com>
2024-04-08 22:42:09 +0100

    Create saida_model.dart

---
+++ lib/model/saida_model.dart
@@ -0,0 +1,30 @@
+import 'package:minhas_financas/model/base_dados.dart';
+
+class Saida {
+  int? id;
+  String? nome;
+  double? valor;
+  DateTime? dataHora;
+
+  Saida({this.nome, this.valor, this.dataHora, this.id});
+  Future<void> adicionar() async {
+    final db = await BaseDados().conectar();
+
+    await db.insert("saida", {
+      'nome': nome,
+      'valor': valor,
+      'dataHora': dataHora.toString(),
+    });
+  }
+
+  Future<void> eliminar() async {
+    final db = await BaseDados().conectar();
+    await db.delete("saida", where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<List> obterTodos() async {
+    final db = await BaseDados().conectar();
+    List data = await db.query("saida");
+    return data;
+  }
+}
