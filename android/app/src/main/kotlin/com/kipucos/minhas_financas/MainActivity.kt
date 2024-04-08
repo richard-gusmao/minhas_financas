9c26ed2e6d8daa159e47a7d7dacacaa5c89d20d9
Richard Gusmao <richardgusmaoalberto@gmail.com>
2024-04-08 19:03:22 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/kipucos/minhas_financas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kipucos.minhas_financas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
